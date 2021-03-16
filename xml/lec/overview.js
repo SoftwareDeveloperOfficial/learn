515f50c63cbac8e960f97ce16e6a3e8d86a9a6b9
SoftwareDeveloperOfficial <79541042+SoftwareDeveloperOfficial@users.noreply.github.com>
2021-03-16 23:05:54 +0530

    Add files via upload

---
+++ xml/lec/overview.js
@@ -0,0 +1,53 @@
+function myFunction1() {
+    location.replace("1)intro.html")
+  }
+
+  function myFunction2() {
+    location.replace("2)xmlFeatures.html")
+  }
+
+  function myFunction3() {
+    location.replace("3)Adv_Disadv.html")
+  }
+
+  function myFunction4() {
+    location.replace("4)html_vs_xml.html")
+  }
+
+  function myFunction5() {
+    location.replace("5)json_vs_xml.html")
+  }
+
+  function myFunction6() {
+    location.replace("./lec/1)intro.html")
+  }
+
+  function myFunction7() {
+    location.replace("./lec/1)intro.html")
+  }
+
+  function myFunction8() {
+    location.replace("./lec/1)intro.html")
+  }
+
+  function myFunction9() {
+    location.replace("./lec/1)intro.html")
+  }
+
+  function myFunction10() {
+    location.replace("./lec/1)intro.html")
+  }
+
+  function myFunction11() {
+    location.replace("./lec/1)intro.html")
+  }
+
+  function myFunction12() {
+    location.replace("./lec/1)intro.html")
+  }
+
+  function myFunction13() {
+    location.replace("./lec/1)intro.html")
+  }
+
+  
---
+++ xml/overview.js
@@ -0,0 +1,53 @@
+function myFunction1() {
+    location.replace("./lec/1)intro.html")
+  }
+
+  function myFunction2() {
+    location.replace("./lec/2)xmlFeatures.html")
+  }
+
+  function myFunction3() {
+    location.replace("./lec/3)Adv_Disadv.html")
+  }
+
+  function myFunction4() {
+    location.replace("./lec/4)html_vs_xml.html")
+  }
+
+  function myFunction5() {
+    location.replace("./lec/5)json_vs_xml.html")
+  }
+
+  function myFunction6() {
+    location.replace("./lec/1)intro.html")
+  }
+
+  function myFunction7() {
+    location.replace("./lec/1)intro.html")
+  }
+
+  function myFunction8() {
+    location.replace("./lec/1)intro.html")
+  }
+
+  function myFunction9() {
+    location.replace("./lec/1)intro.html")
+  }
+
+  function myFunction10() {
+    location.replace("./lec/1)intro.html")
+  }
+
+  function myFunction11() {
+    location.replace("./lec/1)intro.html")
+  }
+
+  function myFunction12() {
+    location.replace("./lec/1)intro.html")
+  }
+
+  function myFunction13() {
+    location.replace("./lec/1)intro.html")
+  }
+
+  
