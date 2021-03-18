5a30e00621c63a3dd6df56250cc70b6796529365
SoftwareDeveloperOfficial <79541042+SoftwareDeveloperOfficial@users.noreply.github.com>
2021-03-18 14:30:01 +0530

    Add files via upload

---
+++ Spring Framework in Easy Steps/lec/overview.js
@@ -0,0 +1,23 @@
+function myFunction1() {
+  location.replace("1)intro.html")
+}
+
+function myFunction2() {
+  location.replace("2)webserviceoverview.html")
+}
+
+function myFunction3() {
+  location.replace("../quiz/quiz1.html")
+}
+
+function myFunction4() {
+  location.replace("4)soapandwebservices.html")
+}
+
+function myFunction5() {
+  location.replace("../quiz/quiz2.html")
+}
+
+function myFunction6() {
+  location.replace("6)summary.html")
+}
