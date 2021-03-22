91b30144b5de6f7966547a318165dc0869102c31
SoftwareDeveloperOfficial <79541042+SoftwareDeveloperOfficial@users.noreply.github.com>
2021-03-22 19:14:55 +0530

    Add files via upload

---
+++ TIBCO/lec/overview.js
@@ -0,0 +1,62 @@
+function myFunction1() {
+  location.replace("day-1.html")
+}
+
+function myFunction2() {
+  location.replace("day-2.html")
+}
+
+function myFunction3() {
+  location.replace("day-3.html")
+}
+
+function myFunction4() {
+  location.replace("day-4.html")
+}
+
+function myFunction5() {
+  location.replace("day-5.html")
+}
+
+function myFunction6() {
+  location.replace("day-6.html")
+}
+function myFunction7() {
+  location.replace("day-7.html")
+}
+
+function myFunction8() {
+  location.replace("day-8.html")
+}
+
+function myFunction9() {
+  location.replace("day-9.html")
+}
+
+function myFunction10() {
+  location.replace("day-10.html")
+}
+
+function myFunction11() {
+  location.replace("day-11.html")
+}
+
+function myFunction12() {
+  location.replace("day-12.html")
+}
+
+function myFunction13() {
+  location.replace("day-13.html")
+}
+
+function myFunction14() {
+  location.replace("day-14.html")
+}
+
+function myFunction15() {
+  location.replace("day-15.html")
+}
+
+function myFunction16() {
+  location.replace("day-16.html")
+}
---
+++ TIBCO/overview.js
@@ -0,0 +1,62 @@
+function myFunction1() {
+  location.replace("lec/day-1.html")
+}
+
+function myFunction2() {
+  location.replace("lec/day-2.html")
+}
+
+function myFunction3() {
+  location.replace("lec/day-3.html")
+}
+
+function myFunction4() {
+  location.replace("lec/day-4.html")
+}
+
+function myFunction5() {
+  location.replace("lec/day-5.html")
+}
+
+function myFunction6() {
+  location.replace("lec/day-6.html")
+}
+function myFunction7() {
+  location.replace("lec/day-7.html")
+}
+
+function myFunction8() {
+  location.replace("lec/day-8.html")
+}
+
+function myFunction9() {
+  location.replace("lec/day-9.html")
+}
+
+function myFunction10() {
+  location.replace("lec/day-10.html")
+}
+
+function myFunction11() {
+  location.replace("lec/day-11.html")
+}
+
+function myFunction12() {
+  location.replace("lec/day-12.html")
+}
+
+function myFunction13() {
+  location.replace("lec/day-13.html")
+}
+
+function myFunction14() {
+  location.replace("lec/day-14.html")
+}
+
+function myFunction15() {
+  location.replace("lec/day-15.html")
+}
+
+function myFunction16() {
+  location.replace("lec/day-16.html")
+}
